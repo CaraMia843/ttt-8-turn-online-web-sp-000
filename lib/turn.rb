72abc122877412eae0760c36523c4d98a246746f
Cara Zellner <carajeanine@gmail.com>
2020-10-06 19:28:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(board, index, player = "X")
+def input_to_index(user_input)
   user_input = user_put.to_i - 1
 end
